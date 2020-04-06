5bbaf56d1984f489253a5b67feb01d3951a8df73
MaxymRomanchuk <maximus234786@gmail.com>
2020-04-06 17:20:18 +0300

    Trying (3)

--- EKino/settings.py
+++ EKino/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-# import django_heroku
+import django_heroku
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -153,4 +153,4 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR,'EKino/static')
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
 
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
