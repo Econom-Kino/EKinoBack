c5364d4e9a50b37a6a10bd22e62c1b7e21a8a0bd
MaxymRomanchuk <maximus234786@gmail.com>
2020-04-11 19:34:08 +0300

    Try of unDebugging(5)

--- EKino/settings.py
+++ EKino/settings.py
@@ -127,7 +127,8 @@ STATIC_URL = '/static/'
 # #location where django collect all static files
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 # # location where you will store your static files
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'ekinoback/static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'staticfiles'),
 ]
 
 django_heroku.settings(locals())
