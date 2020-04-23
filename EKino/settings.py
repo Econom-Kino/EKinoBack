bc67d44463338ad96b5cd93ec712bb4c78432a71
MaxymRomanchuk <maximus234786@gmail.com>
2020-04-23 17:46:31 +0300

    Fixed internal issues

--- EKino/settings.py
+++ EKino/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = '=j7j-_3as2g8z$h=6)sg29i!&3-wt9nml_qg+%5gcg&0bca(_*'
 # SESSION_COOKIE_SECURE = True
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 CORS_ORIGIN_ALLOW_ALL = True
 # SECRET_KEY = os.environ.get('SECRET_KEY')
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
